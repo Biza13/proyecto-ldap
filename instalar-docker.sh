8582fd145ba190386b26014896fff6c0b85cdcd6
Biza_13 <serrano_0_1@hotmail.com>
2025-02-14 17:23:03 +0100

    cambiar comandos de el script de la instancia

--- instalar-docker.sh
+++ instalar-docker.sh
@@ -3,13 +3,13 @@ sudo apt update -y
 sudo apt install -y docker.io
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo usermod -aG docker usuario
+sudo usermod -aG docker ubuntu
 newgrp docker
+sudo apt install awscli -y
 sudo apt install  git -y
 sudo apt install wget -y
 # Descargar Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.27.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # Hacer que Docker Compose sea ejecutable
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo apt install awscli -y

eadf5d024f859231e57ff5bee3c239e86bca9146
Biza_13 <serrano_0_1@hotmail.com>
2025-02-21 18:20:41 +0100

    cambiando dockerfile y sh

--- crearContenedores.sh
+++ crearContenedores.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#source ~/.bashrc
+sleep 30
+sleep 30
+source ~/.bashrc
 sudo docker build -t ldap-img -f ./Dockerfile.LDAP .
 sudo docker run -d --network my-network --name ldap-container -p 636:636 -p 389:389 -e LDAP_ADMIN_PASSWORD="admin" ldap-img
-sleep 30
 sudo docker build -t img-apache -f ./Dockerfile.web .
 sudo docker run -d --network my-network --name apache-container -p 443:443 -p 80:80 img-apache
 #sudo docker run -d --network my-network --name apache-container -p 8080:80 img-apache
